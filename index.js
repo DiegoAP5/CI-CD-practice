932d9ae868e2feb360e97aa44f6e49619f5bfae7
DiegoAP5 <213475@ids.upchiapas.edu.mx>
2024-06-03 21:36:35 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/v1/welcome', (req, res) => {
+    res.send('Hello world');
+})
+
+//add a listen port 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
