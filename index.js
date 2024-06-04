5f76d7c721542ada9c2bdde4dcb3a6f0e93025d2
DiegoAP5 <213475@ids.upchiapas.edu.mx>
2024-06-03 22:26:43 -0600

    teste 2

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Ola');
+    res.send('Prueba 2 para ver si jala');
 })
 
-//add a listen port 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
