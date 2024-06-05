24716fa94f43a0b139f25ee37d6be37460486c78
DiegoAP5 <213475@ids.upchiapas.edu.mx>
2024-06-05 15:07:33 -0600

    last commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('No funciona v1');
+    res.send('No funciona v1  .....................');
 })
 
 app.listen(port, () => {
